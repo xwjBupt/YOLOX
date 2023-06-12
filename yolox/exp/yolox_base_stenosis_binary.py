c0a9679ca35a53924b23a65a093046a2a61f1f1a
xwjBupt <xwj_bupt@163.com>
2023-06-12 21:11:00 +0800

    YOLOX-BaseRun-ALL-NMS0.35

--- yolox/exp/yolox_base_stenosis_binary.py
+++ yolox/exp/yolox_base_stenosis_binary.py
@@ -11,17 +11,19 @@ class Exp(MyExp):
         super(Exp, self).__init__()
         self.depth = 1.33
         self.width = 1.25
-
+        self.nmsthre = 0.35
+        self.input_size = (1024, 1024)
+        self.multiscale_range = 8
         # Define yourself dataset path
         self.data_dir = "/ai/mnt/data/stenosis/selected/Binary/FOLD0/"
         self.train_ann = "train_binary.json"
         self.val_ann = "val_binary.json"
         self.test_ann = "val_binary.json"
         self.fold = "FOLD0"
-        self.exp_name = "YOLOX-BaseRun-ALL"
+        self.exp_name = "YOLOX-BaseRun-ALL-NMS0.35"
         self.output_dir = os.path.join("/ai/mnt/code/YOLOX/output_runs", self.exp_name)
         self.num_classes = 1
 
-        self.max_epoch = 300
+        self.max_epoch = 200
         self.data_num_workers = 4
         self.eval_interval = 1

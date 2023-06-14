b335d83cdee328ee0d44d0581b2bd4e8c7290fb6
xwjBupt <xwj_bupt@163.com>
2023-06-14 11:02:13 +0800

    06_14-11_02@YOLOX-ALL-NMS0.25-V1024-ExBox30-SR8

--- 06_14-10_58@YOLOX-ALL-NMS0.25-V1024-ExBox30-SR16/yolox_base_stenosis_binary.py
+++ 06_14-10_58@YOLOX-ALL-NMS0.25-V1024-ExBox30-SR16/yolox_base_stenosis_binary.py
@@ -14,17 +14,17 @@ class Exp(MyExp):
         self.nmsthre = 0.25
         self.input_size = (1024, 1024)
         self.test_size = (1024, 1024)
-        self.multiscale_range = 8
+        self.multiscale_range = 16
         # Define yourself dataset path
         self.data_dir = "/ai/mnt/data/stenosis/selected/Binary/FOLD0/"
         self.train_ann = "train_binary.json"
         self.val_ann = "val_binary.json"
         self.test_ann = "val_binary.json"
         self.fold = "FOLD0"
-        self.exp_name = "YOLOX-ALL-NMS0.25-V1024-ExBox30-SR8"
+        self.exp_name = "YOLOX-ALL-NMS0.25-V1024-ExBox30-SR16"
         self.output_dir = os.path.join("/ai/mnt/code/YOLOX/output_runs", self.exp_name)
         self.num_classes = 1
-        self.test_conf = 0.01
+        self.test_conf = 0.005
         self.max_epoch = 200
         self.data_num_workers = 4
         self.eval_interval = 1

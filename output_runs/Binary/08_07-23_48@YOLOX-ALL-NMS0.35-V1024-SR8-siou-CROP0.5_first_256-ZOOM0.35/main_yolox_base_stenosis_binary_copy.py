507a5255a3b2a043b4830c47f624daaa0b52b9a4
xwjBupt <xwj_bupt@163.com>
2023-08-08 10:13:25 +0800

    08_08-10_12@YOLOX-ALL-NMS0.35-V1024-SR8-siou-CROP0.5_first_256-ZOOM0.35-MOTION.04

--- output_runs/Binary/08_07-23_48@YOLOX-ALL-NMS0.35-V1024-SR8-siou-CROP0.5_first_256-ZOOM0.35/main_yolox_base_stenosis_binary_copy.py
+++ output_runs/Binary/08_07-23_48@YOLOX-ALL-NMS0.35-V1024-SR8-siou-CROP0.5_first_256-ZOOM0.35/main_yolox_base_stenosis_binary_copy.py
@@ -60,7 +60,7 @@ class Exp(MyExp):
             blur_limit=7, allow_shifted=True, always_apply=False, p=0.35  # 0.35
         )
         self.motion_blur_dict = dict(
-            max_factor=1.31, step_factor=(0.01, 0.03), always_apply=False, p=0.4  # 0.4
+            max_factor=1.31, step_factor=(0.01, 0.03), always_apply=False, p=0  # 0.4
         )
         self.cut_copy_dict = dict(
             iou_thresh=0.2, paste_number=10, thresh=64, expand=5, p=0  # 0.25
@@ -75,8 +75,7 @@ class Exp(MyExp):
         self.cal_thresh = 1.5
         self.ssim_size = (32, 32)
         self.exp_name = (
-            "%s-ALL-NMS0.35-V1024-SR8-siou-CROP0.5_first_256-ZOOM0.35-MOTION.04"
-            % MODELNAME
+            "%s-ALL-NMS0.35-V1024-SR8-siou-CROP0.5_first_256-ZOOM0.35" % MODELNAME
         )
         # ALL-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-MOTION.04-tf5e_3-cutcopy_ex5-clip0.25-siou-fixed_iou_similarity0.3_V32"
         self.output_dir = os.path.join(

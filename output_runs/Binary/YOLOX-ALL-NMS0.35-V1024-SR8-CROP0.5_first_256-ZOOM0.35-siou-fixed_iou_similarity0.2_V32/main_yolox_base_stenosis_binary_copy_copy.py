26f42b819f0c86cad08619bf1f04c2ad34c2d5f9
xwjBupt <xwj_bupt@163.com>
2023-08-20 09:10:19 +0800

    08_20-09_09@YOLOX-DEGREE-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-MOTION0.15-tf5e_3-cutcopy_ex5_0.15-siou

--- output_runs/Binary/YOLOX-ALL-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-siou-fixed_iou_similarity0.2_V32/main_yolox_base_stenosis_binary_copy_copy.py
+++ output_runs/Binary/YOLOX-ALL-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-siou-fixed_iou_similarity0.2_V32/main_yolox_base_stenosis_binary_copy_copy.py
@@ -25,8 +25,8 @@ class Exp(MyExp):
         elif MODELNAME == "YOLOX#M":
             self.depth = 0.67
             self.width = 0.75
-            self.data_num_workers = 8
-            self.batch_size = 4
+            self.data_num_workers = 12
+            self.batch_size = 6
             self.ckpt = "/ai/mnt/code/YOLOX/yolox/models/yolox_m.pth"
         elif MODELNAME == "YOLOX#S":
             self.depth = 0.33
@@ -43,6 +43,7 @@ class Exp(MyExp):
         else:
             assert False, "{} do not support yet"
         self.nmsthre = 0.35
+        self.iou_type = "siou"
         self.input_size = (1024, 1024)
         self.test_size = (1024, 1024)
         self.multiscale_range = 8
@@ -53,40 +54,56 @@ class Exp(MyExp):
             min_area=64,
             min_visibility=0,
             format="coco",
-            random_rate=0.5,
+            random_rate=0.5,  # 0.5
         )
         self.zoom_blur_dict = dict(
-            blur_limit=7, allow_shifted=True, always_apply=False, p=0.35
+            blur_limit=7, allow_shifted=True, always_apply=False, p=0.35  # 0.35
         )
         self.motion_blur_dict = dict(
-            max_factor=1.31, step_factor=(0.01, 0.03), always_apply=False, p=0.15
+            max_factor=1.31, step_factor=(0.01, 0.03), always_apply=False, p=0  # 0.4
         )
         self.cut_copy_dict = dict(
-            iou_thresh=0.2, paste_number=10, thresh=64, expand=5, p=0.15
+            iou_thresh=0.2, paste_number=10, thresh=64, expand=5, p=0  # 0.25
         )
-        self.clip_dict = dict(low=48, high=192, p=0)
+        self.clip_dict = dict(low=48, high=192, p=0)  # 0.25
         # Define yourself dataset path
-        self.data_dir = "/ai/mnt/data/stenosis/selected/Degree/FOLD0/"
-        self.train_ann = "train_degree.json"
-        self.val_ann = "val_degree.json"
-        self.test_ann = "val_degree.json"
+        self.data_dir = "/ai/mnt/data/stenosis/selected/Binary/FOLD0/COCO"
+        self.train_ann = "train_binary.json"
+        self.val_ann = "val_binary.json"
+        self.test_ann = "val_binary.json"
         self.fold = "FOLD0"
-        self.cal_thresh = 1.5
+        self.cal_thresh = 0.2
         self.ssim_size = (32, 32)
         self.exp_name = (
-            "%s-DEGREE-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-MOTION0.15-tf5e_3-cutcopy_ex5_0.15-siou"
+            "%s-ALL-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-siou-fixed_iou_similarity0.2_V32"
             % MODELNAME
         )
+        # ALL-NMS0.35-V1024-SR8-CROP0.5_first_256-ZOOM0.35-MOTION.04-tf5e_3-cutcopy_ex5-clip0.25-siou-fixed_iou_similarity0.3_V32"
         self.output_dir = os.path.join(
-            "/ai/mnt/code/YOLOX/output_runs/Degree", self.exp_name
+            "/ai/mnt/code/YOLOX/output_runs/Binary", self.exp_name
         )
-        self.num_classes = 3
+        self.num_classes = 1
         self.max_epoch = 100
+        self.data_num_workers = 4
+        self.batch_size = 2
         self.test_conf = 0.005
         self.eval_interval = 1
         self.print_interval = 150
-        self.iou_type = "siou"
-        self.box_contain_thresh = 0.1
-        self.use_cab = False
-
-        # self.basic_lr_per_img = 0.01 / 16.0  # 0.01 / 64.0
+        ######
+        # self.mosaic_prob = 0
+        # # prob of applying mixup aug
+        # self.mixup_prob = 0
+        # # prob of applying hsv aug
+        # self.hsv_prob = 0
+        # # prob of applying flip aug
+        # self.flip_prob = 0
+        # # rotation angle range, for example, if set to 2, the true range is (-2, 2)
+        # self.degrees = 0
+        # # translate range, for example, if set to 0.1, the true range is (-0.1, 0.1)
+        # self.translate = 0
+        # self.mosaic_scale = (0.1, 2)
+        # # apply mixup aug or not
+        # self.enable_mixup = False
+        # self.mixup_scale = (0.5, 1.5)
+        # # shear angle range, for example, if set to 2, the true range is (-2, 2)
+        # self.shear = 0
